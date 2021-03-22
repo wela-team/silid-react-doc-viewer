2190181cf2c6326c5da0ebe007e2631ec7867028
cavinpabua <cavinpabua28@gmail.com>
2021-03-22 17:07:43 +0800

    build

---
+++ src/state/index.tsx
@@ -0,0 +1,48 @@
+import React, {
+  createContext,
+  Dispatch,
+  FC,
+  useEffect,
+  useReducer,
+} from "react";
+import { DocViewerProps } from "..";
+import { MainStateActions, setAllDocuments, setMainConfig } from "./actions";
+import {
+  IMainState,
+  initialState,
+  mainStateReducer,
+  MainStateReducer,
+} from "./reducer";
+
+const DocViewerContext = createContext<{
+  state: IMainState;
+  dispatch: Dispatch<MainStateActions>;
+}>({ state: initialState, dispatch: () => null });
+
+const AppProvider: FC<DocViewerProps> = (props) => {
+  const { children, documents, config, pluginRenderers, initialFileNo = 0 } = props;
+
+  const [state, dispatch] = useReducer<MainStateReducer>(mainStateReducer, {
+    ...initialState,
+    currentFileNo: initialFileNo,
+    documents: documents || [],
+    currentDocument: documents && documents.length ? documents[0] : undefined,
+    config,
+    pluginRenderers,
+  });
+
+  // On inital load, and whenever they change,
+  // replace documents with the new props passed in
+  useEffect(() => {
+    dispatch(setAllDocuments(documents));
+    config && dispatch(setMainConfig(config));
+  }, [documents]);
+
+  return (
+    <DocViewerContext.Provider value={{ state, dispatch }}>
+      {children}
+    </DocViewerContext.Provider>
+  );
+};
+
+export { DocViewerContext, AppProvider };
