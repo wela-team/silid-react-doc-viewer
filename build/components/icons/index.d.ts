bfa2a9d8ac273cc6dd8836c22405efddbd580d62
reynaldojrtan29 <rtan@wela.online>
2024-01-05 11:18:44 +0800

    new build

---
+++ build/components/icons/index.d.ts
@@ -0,0 +1,8 @@
+export interface IIconProps {
+    color?: string;
+    size?: string | number | (string & {}) | undefined;
+    reverse?: boolean;
+}
+export declare const PrevDocIcon: (props: IIconProps) => JSX.Element;
+export declare const NextDocIcon: (props: IIconProps) => JSX.Element;
+export declare const LoadingIcon: (props: IIconProps) => JSX.Element;
